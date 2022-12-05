23780838c0dfec18c89073d2c978cf54b6c01c68
Brokkoly <brendankrull@gmail.com>
2022-12-05 12:09:18 -0600

    do day 4

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -11,7 +11,7 @@ const inputPairs = input.map(row => {
 })
 //const inputNums = input.map((str) => Number.parseInt(str));
 const output1 = aocD4Q1(inputPairs);
-const output2 = aocD4Q2(input);
+const output2 = aocD4Q2(inputPairs);
 console.log(output1);
 console.log(output2);
 
@@ -24,6 +24,22 @@ function checkEnvelop(elf1: number[], elf2: number[]): boolean {
   }
   return false;
 
+}
+
+function checkOverlap(elf1: number[], elf2: number[]): boolean {
+  if (elf1[0] >= elf2[0] && elf1[0] <= elf2[1]) {
+    return true;
+  }
+  if (elf1[1] <= elf2[1] && elf1[1] >= elf2[0]) {
+    return true;
+  }
+  if (elf2[0] >= elf1[0] && elf2[0] <= elf1[1]) {
+    return true;
+  }
+  if (elf2[1] <= elf1[1] && elf2[1] >= elf1[0]) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -41,6 +57,12 @@ function aocD4Q1(input: number[][][]): number {
   return numEnvelop;
 }
 
-function aocD4Q2(input: string[]): number {
-  return -1;
+function aocD4Q2(input: number[][][]): number {
+  let numOverlap = 0;
+  input.forEach(pair => {
+    if (checkOverlap(pair[0], pair[1])) {
+      numOverlap++;
+    }
+  })
+  return numOverlap
 }
