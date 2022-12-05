b97aedb26b2be54c42a77897acb9ddb96d57ecd0
Brendan Krull <brendan.krull@oliveai.com>
2022-12-05 13:12:51 -0600

    day 5 done

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -3,78 +3,129 @@ import { Dirent } from "fs";
 import * as helpers from "../helpers";
 import { Stack } from "../helpers";
 
-const input = helpers.readInput("inputs/input5test.txt");
+const input = helpers.readInput("inputs/input5.txt");
 console.log(input);
 
-const inputSplitIndex = input.findIndex(row => row.length === 0)
+const inputSplitIndex = input.findIndex((row) => row.length === 0);
 
 const boxes = input.slice(0, inputSplitIndex);
-const stackIds = boxes[boxes.length - 1].split('   ');
+console.log(boxes);
+const stackIds = boxes[boxes.length - 1].split("   ");
 const numStacks = parseInt(stackIds[stackIds.length - 1]);
 const stacks: Stack<string>[] = new Array<Stack<string>>(numStacks);
+console.log("stacks.length", stacks.length);
 for (let i = 0; i < stacks.length; i++) {
   stacks[i] = new Stack<string>();
 }
-boxes.forEach((row, index) => {
-  if (index === boxes.length - 1) {
+console.log("stacks.length", stacks.length);
+boxes.reverse().forEach((row, index) => {
+  if (index === 0) {
     return;
   }
-  for (let i = 1; i <= numStacks; i++) {
-    if (row.charAt(i)) {
-      //todo: getting undefined error here
-      stacks[i].push(row.charAt(i * 3 + 2));
+  for (let i = 0; i <= stacks.length; i++) {
+    const char = row.charAt(i * 4 + 1);
+    if (char && char !== " ") {
+      if (!stacks[i]) {
+        stacks[i] = new Stack<string>();
+      }
+      stacks[i].push(row.charAt(i * 4 + 1));
     }
   }
 });
+// stacks.forEach((stack) => {
+//   console.log(stack.toArray());
+// });
 
-
-
-
-const directions = input.slice(inputSplitIndex + 1).map(row => {
-  const splitDir = row.split(' ');
+const directions = input.slice(inputSplitIndex + 1).map((row) => {
+  const splitDir = row.split(" ");
   const dir: Direction = {
     num: parseInt(splitDir[1]),
     source: parseInt(splitDir[3]),
-    destination: parseInt(splitDir[5])
-  }
-  return dir
+    destination: parseInt(splitDir[5]),
+  };
+  return dir;
 });
 
-
 type Direction = {
-  num: number,
-  source: number,
-  destination: number
-}
-
-console.log(inputSplitIndex);
+  num: number;
+  source: number;
+  destination: number;
+};
 
 //const inputNums = input.map((str) => Number.parseInt(str));
-const output1 = aocD5Q1(stacks, directions);
-const output2 = aocD5Q2(input);
+const output1 = aocD5Q1(
+  stacks.map((stack) => stack.clone()),
+  directions
+);
+const output2 = aocD5Q2(
+  stacks.map((stack) => stack.clone()),
+  directions
+);
 console.log(output1);
 console.log(output2);
-
 
 /**
  *
  * @param input
  * @returns
  */
-function aocD5Q1(boxes: Stack<string>[], directions: Direction[]): string {
-  directions.forEach(direction => {
+function aocD5Q1(stacks: Stack<string>[], directions: Direction[]): string {
+  // stacks.forEach((stack, index) => {
+  //   console.log(index);
+  //   console.log(stack.toArray());
+  // });
+  directions.forEach((direction) => {
     for (let i = 0; i < direction.num; i++) {
-      const top = boxes[direction.source].pop();
+      const top = stacks[direction.source - 1].pop();
       if (top) {
-        boxes[direction.destination].push;
+        stacks[direction.destination - 1].push(top);
       }
     }
-  })
-  return boxes.map(stack => {
-    return stack.peek();
-  }).join('');
+  });
+  return stacks
+    .map((stack) => {
+      return stack.peek();
+    })
+    .join("");
 }
 
-function aocD5Q2(input: string[]): number {
-  return -1;
+function aocD5Q2(stacks: Stack<string>[], directions: Direction[]): string {
+  stacks.forEach((stack, index) => {
+    //console.log(index);
+    console.log(stack.toArray());
+  });
+  directions.forEach((direction, dirIndex) => {
+    const grabbed = new Stack<string>();
+    for (let i = 0; i < direction.num; i++) {
+      const top = stacks[direction.source - 1].pop();
+      if (top) {
+        grabbed.push(top);
+      }
+    }
+    while (true) {
+      const current = grabbed.pop();
+      if (!current) {
+        break;
+      }
+      stacks[direction.destination - 1].push(current);
+    }
+    console.log(`after direction index ${dirIndex}`);
+    console.log(
+      `move ${direction.num} from ${direction.source} to ${direction.destination}`
+    );
+
+    stacks.forEach((stack, index) => {
+      console.log(stack.toArray());
+    });
+  });
+  console.log();
+  stacks.forEach((stack, index) => {
+    //console.log(index);
+    console.log(stack.toArray());
+  });
+  return stacks
+    .map((stack) => {
+      return stack.peek();
+    })
+    .join("");
 }
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,40 +1,50 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 export function readInput(path: string): string[] {
-	let data: string[] = [];
-	try {
-		data = fs.readFileSync(path, 'utf-8').toString().replace(/\r\n/g, '\n').split('\n');
-		console.log(data);
-	}
-	catch (err) {
-		console.error(err);
-	}
-	return data;
+  let data: string[] = [];
+  try {
+    data = fs
+      .readFileSync(path, "utf-8")
+      .toString()
+      .replace(/\r\n/g, "\n")
+      .split("\n");
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+  return data;
 }
-export class Stack<T>{
-    private storage: T[] = [];
-    constructor(private capacity: number = Infinity) { }
+export class Stack<T> {
+  private storage: T[] = [];
+  constructor(private capacity: number = Infinity) {}
 
-    push(item: T): void {
-        if (this.size() === this.capacity) {
-            throw Error("Stack has reached max capacity, you cannot add more items");
-        }
-        this.storage.push(item);
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("Stack has reached max capacity, you cannot add more items");
     }
+    this.storage.push(item);
+  }
 
-    pop(): T | undefined {
-        return this.storage.pop();
-    }
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
 
-    peek(): T | undefined {
-        return this.storage[this.size() - 1];
-    }
+  peek(): T | undefined {
+    return this.storage[this.size() - 1];
+  }
 
-    size(): number {
-        return this.storage.length;
-    }
-    toArray(): T[] {
-        return [...this.storage];
-    }
+  size(): number {
+    return this.storage.length;
+  }
+  toArray(): T[] {
+    return [...this.storage];
+  }
+  clone(): Stack<T> {
+    const newStack = new Stack<T>(this.capacity);
+    this.storage.forEach((item) => {
+      newStack.push(item);
+    });
+    return newStack;
+  }
 }
 // export async function readInputFromURL(path: string): Promise<string[]> {
 // 	const txt = fetch(path)
@@ -43,5 +53,4 @@ export class Stack<T>{
 // 	return txt;
 //}
 
-
-export { };
+export {};
