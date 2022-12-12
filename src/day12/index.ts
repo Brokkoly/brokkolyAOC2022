28b9f01b1101eb5dfde49a883393f1332e621abb
Brendan Krull <brendan.krull@oliveai.com>
2022-12-12 16:42:21 -0600

    start d12 stuff

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -9,68 +9,32 @@ class Node {
   endNode: boolean;
   elevation: number;
   constructor(character: string) {
-    this.elevation = character.charCodeAt(0) - "a".charCodeAt(0);
+    this.elevation =
+      character === "S"
+        ? "a".charCodeAt(0)
+        : character.charCodeAt(0) - "a".charCodeAt(0);
+    this.endNode = character === "E";
+    this.neighbors = [];
   }
 }
 
-const nodes = new Map<string, INode>();
-
-input.forEach((arr) => {
-  arr.forEach((label) => {
-    if (!nodes.has(label)) {
-      nodes.set(label, {
-        label: label,
-        neighbors: [],
-        endNode: label === "end",
-        small: label === label.toLowerCase() || label === "start",
-      });
-    }
-    if (label === "start") {
-      startNode = nodes.get("start")!;
-    }
-  });
+const inputNodes = input.map((row) => {
+  const rowNodes: Node[] = [];
+  row.forEach((char) => new Node(char));
 });
 
-console.log(`Output 1: ${aocD12Q1(inputMonkeys)}`);
-console.log(`Output 2: ${aocD12Q2(inputMonkeys)}`);
+console.log(`Output 1: ${aocD12Q1(inputNodes)}`);
+console.log(`Output 2: ${aocD12Q2(inputNodes)}`);
 
 /**
  *
  * @param input
  * @returns
  */
-function aocD12Q1(monkies: Monkey[]): number {
-  for (let i = 0; i < 20; i++) {
-    monkies.forEach((monkey) => monkey.takeTurn());
-  }
-  const monkeyActivity = monkies.map((monkey) => monkey.numInspections);
-  var highest: number = 0;
-  var secondHighest: number = 0;
-  monkeyActivity.forEach((num) => {
-    if (num > highest) {
-      secondHighest = highest;
-      highest = num;
-    } else if (num > secondHighest) {
-      secondHighest = num;
-    }
-  });
-  return highest * secondHighest;
+function aocD12Q1(monkies: Node[]): number {
+  return -1;
 }
 
-function aocD12Q2(monkies: Monkey[]): number {
-  for (let i = 0; i < 10000; i++) {
-    monkies.forEach((monkey) => monkey.takeTurn());
-  }
-  const monkeyActivity = monkies.map((monkey) => monkey.numInspections);
-  var highest: number = 0;
-  var secondHighest: number = 0;
-  monkeyActivity.forEach((num) => {
-    if (num > highest) {
-      secondHighest = highest;
-      highest = num;
-    } else if (num > secondHighest) {
-      secondHighest = num;
-    }
-  });
-  return highest * secondHighest;
+function aocD12Q2(monkies: Node[]): number {
+  return -1;
 }
