3dd5ef49ea6ffb3b5021a410126d7925553fb2ca
Brokkoly <brendankrull@gmail.com>
2022-12-12 19:08:06 -0600

    need to implement dijkstra's algorithm

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,4 +1,6 @@
+import { start } from "repl";
 import * as helpers from "../helpers";
+import { Queue } from "../Queue";
 
 const input = helpers
   .readInput("inputs/input12test.txt")
@@ -7,23 +9,63 @@ const input = helpers
 class Node {
   neighbors: Node[];
   endNode: boolean;
+  startNode: boolean;
   elevation: number;
+  distance: number;
+  visited: boolean = false;
   constructor(character: string) {
     this.elevation =
       character === "S"
         ? "a".charCodeAt(0)
-        : character.charCodeAt(0) - "a".charCodeAt(0);
+        : character === "E" ? "z".charCodeAt(0) :
+          character.charCodeAt(0) - "a".charCodeAt(0);
     this.endNode = character === "E";
+    this.startNode = character === "S";
     this.neighbors = [];
+    this.distance = Number.POSITIVE_INFINITY;
+  }
+}
+const inputNodes = input.map((row) => {
+  const rowNodes: Node[] = [];
+  row.forEach((char) => rowNodes.push(new Node(char)));
+  return rowNodes;
+});
+
+var startNode: Node;
+var endNode: Node;
+
+for (let rowIndex = 0; rowIndex < inputNodes.length; rowIndex++) {
+  for (let columnIndex = 0; columnIndex < inputNodes[rowIndex].length; columnIndex++) {
+
+    const node = inputNodes[rowIndex][columnIndex];
+    if (node.startNode) {
+      startNode = node;
+    }
+    if (node.endNode) {
+      endNode = node;
+    }
+    const backNode = inputNodes[rowIndex][columnIndex - 1];
+    const frontNode = inputNodes[rowIndex][columnIndex + 1];
+    const topNode = inputNodes.at(rowIndex - 1)?.at(columnIndex);
+    const bottomNode = inputNodes.at(rowIndex + 1)?.at(columnIndex);
+    node.neighbors.push(...[backNode!, frontNode!, topNode!, bottomNode!].filter(maybeNode => maybeNode !== undefined));
   }
 }
 
-const inputNodes = input.map((row) => {
-  const rowNodes: Node[] = [];
-  row.forEach((char) => new Node(char));
-});
+function allDone(paths: Node[][]): boolean {
+  for (let i = 0; i < paths.length; i++) {
+    if (paths[i].find(node => node.endNode) === undefined) {
+      return false;
+    }
+  }
+  return true;
+}
 
-console.log(`Output 1: ${aocD12Q1(inputNodes)}`);
+// inputNodes.forEach(row => {
+//   console.log(row.map(node => node.neighbors.length).join(''));
+// })
+
+console.log(`Output 1: ${aocD12Q1(startNode!)}`);
 console.log(`Output 2: ${aocD12Q2(inputNodes)}`);
 
 /**
@@ -31,10 +73,12 @@ console.log(`Output 2: ${aocD12Q2(inputNodes)}`);
  * @param input
  * @returns
  */
-function aocD12Q1(monkies: Node[]): number {
+function aocD12Q1(startNode: Node): number {
+  var nodes = [startNode];
+  
   return -1;
 }
 
-function aocD12Q2(monkies: Node[]): number {
+function aocD12Q2(nodes: Node[][]): number {
   return -1;
 }
